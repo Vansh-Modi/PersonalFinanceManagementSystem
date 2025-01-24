06647f804e44fe18dd6f2e05b6d7e5daa22ac6bf
dhameliyatan <tandhameliya0111@gmail.com>
2025-01-24 15:17:45 +0530

    login

---
+++ login.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="PersonalFinanceManagementSystem.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin:auto;
+            width:300px;
+            border: 3px solid #000000;
+            background-color: #c8f3ec;
+        }
+        .auto-style2 {
+            font-size: large;
+            text-align: center;
+        }
+        .auto-style3 {
+            width: 604px;
+            text-align: center;
+        }
+        .auto-style4 {
+            width: 604px;
+            text-align: center;
+            height: 33px;
+        }
+        .auto-style5 {
+            height: 33px;
+        }
+        .auto-style6 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body class="">
+    <h2 class="auto-style6">Log in Here</h2>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2" colspan="2"><strong>Login Page</strong></td>
+                </tr>
+                <tr>
+                    <td class="auto-style3"><strong>Username :</strong></td>
+                    <td>
+                        <asp:TextBox ID="txtusername" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3"><strong>Password :</strong></td>
+                    <td>
+                        <asp:TextBox ID="txtpass" runat="server" TextMode="Password"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Button ID="btnlogin" runat="server" OnClick="btnlogin_Click" Text="Login" />
+                    </td>
+                    <td class="auto-style5">
+                        </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ login.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace PersonalFinanceManagementSystem
+{
+    public partial class login : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["PersonalFinanceManagementSystem"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string query = "SELECT * FROM login WHERE custId = @user AND password = @pass";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@user", txtusername.Text);
+                cmd.Parameters.AddWithValue("@pass", txtpass.Text);
+
+                try
+                {
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        Session["user"] = txtusername.Text;
+                        Response.Redirect("Dashboard.aspx");
+                    }
+                    else
+                    {
+                        Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Login Failed!')</script>");
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", $"<script>alert('SQL Error: {sqlEx.Message}')</script>");
+                }
+                catch (Exception ex)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), $"Scripts", $"<script>alert('Error: {ex.Message}')</script>");
+                }
+                finally
+                {
+                    if (con.State == System.Data.ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+        }
+    }
+}
